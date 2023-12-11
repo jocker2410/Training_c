7e320b2899a3a062ccb46146401362dbe3752804
jocker2410 <jocker2410@gmail.com>
2023-12-11 07:37:47 +0100

    Started with second exercise

---
+++ Fahrenheit-Celsius/fahrenheit-celsius.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+main()
+{
+	float fahr, cel
+	fahr = 0; 
+
+	while (fahr <= 300)
+	{
+		cel = (5/9) * (fahr - 32);
+		print("%4.0f %6.1f\n", fahr, cel);
+		fahr += 20;
+	}	
+
+}
