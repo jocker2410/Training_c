31b6dd8704288af7b38ff54e1838c7c394918e28
jocker2410 <jocker2410@gmail.com>
2023-12-11 08:15:26 +0100

    error fixed

--- Fahrenheit-Celsius/fahrenheit-celsius.c
+++ Fahrenheit-Celsius/fahrenheit-celsius.c
@@ -2,12 +2,13 @@
 
 main()
 {
+
 	float fahr, cel;
 	fahr = 0; 
 
 	while (fahr <= 300)
 	{
-		cel = (5/9) * (fahr - 32);
+		cel = (5.0/9.0) * (fahr - 32.0);
 		printf("%4.0f %6.1f\n", fahr, cel);
 		fahr += 20;
 	}	
