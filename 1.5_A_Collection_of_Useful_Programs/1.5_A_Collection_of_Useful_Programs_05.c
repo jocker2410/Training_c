3a6a72d3f6698d1b1e61472301e9d7289a98f8f3
Author: Coldman <jocker2410@gmail.com>
2023-12-16 14:50:46 +0100

    add file *_05.c

--- 1.5_A_Collection_of_Useful_Programs/1.5_A_Collection_of_Useful_Programs_05.c
+++ 1.5_A_Collection_of_Useful_Programs/1.5_A_Collection_of_Useful_Programs_05.c
@@ -1,21 +1,31 @@
-#include <stdoio.h>
+#include <stdio.h>
+/*Exercise 1-6*/
 
-char user_input[250]; 
-int word_c, input_c; 
+int counter; 
 
 int main()
 {
-	input_c = word_c = 0; 
+	int i;
+	char user_input[] = "Hallo Welt!\n";
+	i = counter = 0; 
 
-	printf("Your text please.");
-	scanf("%249s", user_input);
-	if user_input == "NULL"
+	printf("Your enter your text please.\n\n");
+	
+	
+	if (user_input == "NULL")
 	{
 		printf("[-] No user input, pease try once more!");
-		exit(1);
+		return 1;
 	}
-	while(user_input[i] == '\0')
+	while(user_input[i] != '\0')
 	{
-		if (user_input == ' ' || user_input == '\t') 	
+		if (user_input[i] == ' ' || user_input[i] == '\t' || user_input[i] == '\n')
+		{
+			printf("\n");
+			++counter;
+		}
+		++i;				
 	}
+	printf("blanks, tabs and newlines are: %d\nAnd the set consists of %d caracters", counter, i);
+	return 0;
 }
